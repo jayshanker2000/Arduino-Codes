2dd0e950bca590279ce499a7158e8e3611cabd66
Jay Shankar Pandit <39528562+jayshanker2000@users.noreply.github.com>
2020-10-10 21:21:03 +0530

    Add files via upload

---
+++ motor_driver/motor_driver.ino
@@ -0,0 +1,29 @@
+int dir = 12;
+int pwm = 11;
+
+void setup(){
+
+
+  pinMode(11 , OUTPUT);
+  pinMode(12 , OUTPUT);
+
+
+digitalWrite(dir , LOW);
+digitalWrite(pwm , LOW);
+
+
+
+}
+
+
+void loop(){
+
+digitalWrite(dir , LOW);
+analogWrite(pwm , 255);
+delay(1000);
+digitalWrite(dir , HIGH);
+analogWrite(pwm , 255);
+delay(1000);
+  
+}
+
---
+++ proximity/proximity.ino
@@ -0,0 +1,18 @@
+const int ProxSensor=2;
+
+void setup() { 
+  Serial.begin(9600);               
+  // initialize the digital pin as an output.
+  // Pin 13 has an LED connected on most Arduino boards:
+  pinMode(13, OUTPUT);     
+  //Pin 2 is connected to the output of proximity sensor
+  pinMode(A2,INPUT);
+}
+
+void loop() {
+  int i;
+  i = analogRead(A2);
+  Serial.println(i);
+  
+  delay(100);              // wait for a second
+}
